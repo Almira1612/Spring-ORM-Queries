7ca3c757b09e02cdafd450dc6728cf6fdcae3d48
Aimila Maimaitijiang <almira1612@github.com>
2022-10-29 00:09:44 -0400

    Initial commit

---
+++ src/main/java/com/cydeo/SpringOrmQueriesApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOrmQueriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOrmQueriesApplication.class, args);
+	}
+
+}
