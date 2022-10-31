480593cd7513ae27b77daed668b79de8e39b54f3
Aimila Maimaitijiang <almira1612@github.com>
2022-10-30 23:47:22 -0400

    Added DepartmentRepository

--- src/main/java/com/cydeo/boostrap/DataGenerator.java
+++ src/main/java/com/cydeo/boostrap/DataGenerator.java
@@ -1,11 +1,13 @@
 package com.cydeo.boostrap;
 
+import com.cydeo.repository.DepartmentRepository;
 import com.cydeo.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
 
 public class DataGenerator implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
+    private final DepartmentRepository departmentRepository;
 
     public DataGenerator(RegionRepository regionRepository) {
         this.regionRepository = regionRepository;
@@ -14,11 +16,24 @@ public class DataGenerator implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        System.out.println("-----------------Region Start----------------");
+        System.out.println("-----------------REGION START-----------------");
 
         System.out.println("findByCountry:" + regionRepository.findByCountry("Canada"));
         System.out.println("findDistinctByCountry:" + regionRepository.findDistinctByCountry("Canada"));
+        System.out.println("findByCountryContaining:" + regionRepository.findByCountryContaining("United"));
+        System.out.println("findByCountryContainingOrderByCountry:" + regionRepository.findByCountryContainingOrderByCountry("Asia"));
+        System.out.println("findTopByCountry:" + regionRepository.findTop2ByCountry("Canada"));
 
-        System.out.println("-----------------Region End----------------");
+        System.out.println("-----------------REGION END-----------------");
+
+        System.out.println("-----------------DEPARTMENT START-----------------");
+
+        System.out.println("findByDepartment:" + departmentRepository.findByDepartment("Toys"));
+        System.out.println("findByDivisionIs:" + departmentRepository.findByDivisionIs("Outdoors"));
+        System.out.println("findDistinctTop3ByDivisionContains:" + departmentRepository.findDistinctTop3ByDivisionContains("Hea"));
+
+
+
+        System.out.println("-----------------DEPARTMENT END-----------------");
     }
 }
