54694e4de3fe0879f86e84ee1b3fbca2dd649be6
Aimila Maimaitijiang <almira1612@github.com>
2022-11-02 23:43:34 -0400

    Created CourseRepository class

---
+++ src/main/java/com/cydeo/repository/CourseRepository.java
@@ -0,0 +1,10 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    //find all courses by category
+
+}
