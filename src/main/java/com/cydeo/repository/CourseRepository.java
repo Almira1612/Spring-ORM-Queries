df099f6b8e9cb16482095c514ed88b1f028a7640
Aimila Maimaitijiang <almira1612@github.com>
2022-11-03 00:05:17 -0400

    added query

--- src/main/java/com/cydeo/repository/CourseRepository.java
+++ src/main/java/com/cydeo/repository/CourseRepository.java
@@ -10,5 +10,9 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
     //find all courses by category
     List<Course> findByCategory(String category);
 
+    //Find all courses by Category and order the entities by name
+    List<Course> findByCategoryByName(String category);
 
+    //Checks if a course with the provided name exists. Return true if course exists, false otherwise
+    boolean existsByName(String name);
 }
