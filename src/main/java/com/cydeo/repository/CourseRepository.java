ab03ffb28cf75ade562e72eb0b041cb7435d5a36
Aimila Maimaitijiang <almira1612@github.com>
2022-11-02 23:51:14 -0400

    Edit output on DataGenerator

--- src/main/java/com/cydeo/repository/CourseRepository.java
+++ src/main/java/com/cydeo/repository/CourseRepository.java
@@ -3,8 +3,12 @@ package com.cydeo.repository;
 import com.cydeo.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
     //find all courses by category
+    List<Course> findByCategory(String category);
+
 
 }
